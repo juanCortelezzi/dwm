bddf09fc8800fc5ba9227b7bf481c832c64caf43
juancortelezzi <juanbicortelezzi@gmail.com>
2021-08-27 14:37:57 -0300

    added reshut command with args

--- config.def.h
+++ config.def.h
@@ -67,6 +67,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dapps[] = { "dapps", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *reshut[] = { "reshut", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
@@ -124,7 +125,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      spawn,          SHCMD("killall xinit") },
-	{ MODKEY|ShiftMask,			    XK_x,      spawn,	       SHCMD("reshut")},
+	{ MODKEY|ShiftMask,			    XK_x,      spawn,	       {.v = reshut }},
 };
 
 /* button definitions */
